c422b3acc29281c0f90bcaaea350fbe53c79d6f3
nu0786 <nu0786@gmail.com>
2022-05-29 18:55:45 +0530

    Add files via upload
    laravel test

---
+++ app/Models/Event.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Models;
+
+
+use Illuminate\Database\Eloquent\Model;
+
+class Event extends Model
+{
+
+	public function workshops()
+    {
+        return $this->hasMany('App\Models\Workshop', 'event_id');
+    }
+
+}
